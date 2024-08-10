300cfcab48762840a08ef6d9c26c07305cedb4fc
Randolf <1665718426@qq.com>
2024-08-10 21:17:47 +0800

    补充搜索文本更新ListBox

--- src/TwincatToolbox/ViewModels/DataLogViewModel.cs
+++ src/TwincatToolbox/ViewModels/DataLogViewModel.cs
@@ -1,20 +1,11 @@
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Xml.Linq;
-
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using Material.Icons;
-
-using TwinCAT.Ads.TypeSystem;
-using TwinCAT.TypeSystem;
 using TwincatToolbox.Models;
-using TwincatToolbox.Services;
 using TwincatToolbox.Services.IService;
 
 namespace TwincatToolbox.ViewModels;
@@ -22,6 +13,7 @@ namespace TwincatToolbox.ViewModels;
 public partial class DataLogViewModel(IAdsComService adsComService) : ViewModelBase("DataLog", MaterialIconKind.Blog)
 {
     private readonly IAdsComService _adsComService = adsComService;
+    [ObservableProperty] private string _searchText = string.Empty;
 
     public ObservableCollection<SymbolInfo> AvailableSymbols { get; } = [];
     public ObservableCollection<SymbolInfo> RecordSymbols { get; } = [];
@@ -43,6 +35,5 @@ public partial class DataLogViewModel(IAdsComService adsComService) : ViewModelB
         }
 
         Debug.WriteLine("Available symbols: {0}", symbols.Count());
-
     }
 }
